56a44ff282eddef4efa0c2d5fa84bae582f8d52b
戚晓龙 <2425532357@qq.com>
2025-03-22 11:47:17 +0800

    消除报错

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 // Reexport the native module. On web, it will be resolved to ExpoAliyunPushModule.web.ts
 // and on native platforms to ExpoAliyunPushModule.ts
 export { default } from './ExpoAliyunPushModule';
-export { default as ExpoAliyunPushView } from './ExpoAliyunPushView';
 export * from  './ExpoAliyunPush.types';
